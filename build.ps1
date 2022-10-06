fd56366a74be7a184b2f5622bf50e2fda6a4261c
Nils Andresen <nils@nils-andresen.de>
2022-10-06 11:00:43 +0200

    (maint) update Cake.Recipe in CI to v 3.0.1
    and Cake to 1.3.0

--- build.ps1
+++ build.ps1
@@ -9,6 +9,5 @@ $env:DOTNET_NOLOGO = '1'
 dotnet tool restore
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
-dotnet cake recipe.cake --bootstrap
 dotnet cake recipe.cake @args
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
